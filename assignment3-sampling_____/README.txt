0ed03aea917404d3322313942748af6955104f73
Zhongjie Yu <zhongjie@wks-12-55>
2016-06-22 16:45:25 +0200

    fffffff

--- assignment3-sampling_____/README.txt
+++
@@ -1,4 +0,0 @@
-Please run script4run.m to see the results.
-
-By Zhongjie YU & Xiaoyu SHEN
-SID 2557816 & 2561562
--- assignment3-sampling_____/pstar.m
+++
@@ -1,7 +0,0 @@
-function px = pstar( x,w1,w2,miu1,miu2,sigma1,sigma2)
-% mixture Gaussian density function
-%   Detailed explanation goes here
-px = w1*1/sqrt(2*pi*sigma1)*exp(-((x-miu1)^2)/(2*sigma1^2))+w2*1/sqrt(2*pi*sigma2)*exp(-((x-miu2)^2)/(2*sigma2^2));
-
-end
-
